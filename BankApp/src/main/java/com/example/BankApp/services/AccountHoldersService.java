a186f899063355c0cee63ed0035edc044b5972ed
Miguel <blanco.jimenez.1992@gmail.com>
2022-12-14 16:37:30 +0100

    Update AccountHoldersService.java

--- BankApp/src/main/java/com/example/BankApp/services/AccountHoldersService.java
+++ BankApp/src/main/java/com/example/BankApp/services/AccountHoldersService.java
@@ -1,6 +1,7 @@
 package com.example.BankApp.services;
 
 
+import com.example.BankApp.dto.TransferDto;
 import com.example.BankApp.models.Account;
 import com.example.BankApp.repositories.AccountHolderRepository;
 import com.example.BankApp.repositories.AccountRepository;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.crypto.SecretKey;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class AccountHoldersService{
@@ -26,6 +28,22 @@ public class AccountHoldersService{
 
     public Account getAccount (long id, String secretKey){
        return accountRepository.findByIdAndSecretKey(id,secretKey);
+    }
+
+    public void transfer (TransferDto transferDto){
+        BigDecimal money = transferDto.getMoneyToTransfer();
+        Long IdmoneyTo = transferDto.getToAccountId();
+        Long IdmoneyFrom = transferDto.getFromAccountId();
+        Account accountTo = accountRepository.getReferenceById(IdmoneyTo);
+        Account accountFrom = accountRepository.getReferenceById(IdmoneyFrom);
+        BigDecimal AccountMoneyFrom = accountFrom.getBalance();
+        BigDecimal AccountMoneyTo = accountTo.getBalance();
+        AccountMoneyFrom.add(money);
+        AccountMoneyTo.subtract(money);
+        accountTo.setBalance(AccountMoneyTo);
+        accountFrom.setBalance(AccountMoneyFrom);
+        accountRepository.save(accountTo);
+        accountRepository.save(accountFrom);
     }
 
 
